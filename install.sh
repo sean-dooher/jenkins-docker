cd43cd6b373e78f283991d2f44f742a02f63b659
Sean Dooher <sean@dooher.net>
2018-07-28 00:14:07 -0700

    Add initial install script and configurations

--- install.sh
+++ install.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
+
+if [ -z "$JENKINS_HOST" ]; then
+	echo "Enter your domain name:"
+	read JENKINS_HOST
+fi
+
+if [ -z "$USER_EMAIL" ]; then
+	echo "Enter your email for letsencrypt:"
+	read USER_EMAIL
+fi
+
+mkdir -p /letsencrypt-site
+
+docker-compose -f initial-compose.yml up -d
+
+docker run -it --rm \
+-v /docker-volumes/etc/letsencrypt:/etc/letsencrypt \
+-v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
+-v /letsencrypt-site:/data/letsencrypt \
+-v "/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
+certbot/certbot \
+certonly --webroot \
+--email $USER_EMAIL --agree-tos --no-eff-email \
+--webroot-path=/data/letsencrypt \
+-d $JENKINS_HOST
+
+docker-compose -f initial-compose.yml down
+
 export DOCKER_COMPOSE=$(which docker-compose)
 envsubst < jenkins.service.template > /etc/systemd/system/jenkins.service
--- nginx/run_nginx.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-envsubst '\$JENKINS_HOST' < /nginx.conf.template > /etc/nginx/conf.d/default.conf
-exec nginx -g "daemon off;"
